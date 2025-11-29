d851cab23a806cea6f8641a4fd5c3df20b8138f2
Zeidan chabo <zeidanchabo@gmail.com>
2025-11-28 22:18:35 -0500

    modifying accepted origins

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
-    allow_headers=["Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 class Message(BaseModel):
    content:str
